18d64e28c4ae275de156e546d89881d07651cbfe
arthurs14 <arthurcs426@gmail.com>
2021-07-29 11:57:48 -0700

    added some state to the form for the input fields

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,15 +1,28 @@
+import { useState } from 'react';
+
 const AddTransaction = () => {
+  const [formData, setFormData] = useState({
+    text: '',
+    amount: 0,
+  });
+
+  const onChange = (ev) => {
+    setFormData({ ...formData, [ev.target.name]: ev.target.value });
+  };
+
+  console.log(formData);
+
   return (
     <>
       <h3>Add New Transaction</h3>
-      <form id="form">
+      <form>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" id="text" placeholder="Enter Text..." />
+          <input type="text" name="text" value={formData.text} onChange={onChange} placeholder="Enter Text..." />
         </div>
         <div className="form-control">
           <label htmlFor="amount">Amount <br /> (negative - expense, positive - income)</label>
-          <input type="number" id="amount" placeholder="Enter Amount..." />
+          <input type="number" name="amount" value={formData.amount} onChange={onChange} placeholder="Enter Amount..." />
         </div>
         <button className="btn">Add Transaction</button>
       </form>
