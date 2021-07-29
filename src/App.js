e6a44393900aac250ef2b523b9895093cc1c914b
arthurs14 <arthurcs426@gmail.com>
2021-07-29 11:22:05 -0700

    created a header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 
+import Header from './components/Header';
+
 function App() {
   return (
     <div>
-      Hello World!
+      <Header />
+      <div className="container">
+
+      </div>
     </div>
   );
 }
