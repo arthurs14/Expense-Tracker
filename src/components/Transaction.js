5ef28f75efa1592e4e8a952c908d7fdd95d2c8e8
arthurs14 <arthurcs426@gmail.com>
2021-08-02 14:58:51 -0700

    can now fully delete a transaction from the history list

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,11 +1,16 @@
+import { useGlobal } from '../context/GlobalState';
+
 const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useGlobal();
+  const { id, text, amount } = transaction;
+
   const sign = transaction.amount < 0 ? '-' : '+';
   const transactionIndicator = transaction.amount < 0 ? 'minus' : 'plus'
 
   return (
     <li className={transactionIndicator}>
-      {transaction.text} <span>{`${sign}$${Math.abs(transaction.amount)}`}</span>
-      <button className="delete-btn">x</button>
+      {text} <span>{`${sign}$${Math.abs(amount)}`}</span>
+      <button className="delete-btn" onClick={() => deleteTransaction(id)}>x</button>
     </li>
   );
 };
