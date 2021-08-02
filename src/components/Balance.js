8e9c8889ad65601e20abd3620d31a9ee37ce562f
arthurs14 <arthurcs426@gmail.com>
2021-08-02 14:52:05 -0700

    added balance and income expense tracker

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,8 +1,15 @@
+import { useGlobal } from '../context/GlobalState';
+
 const Balance = () => {
+  const { transactions } = useGlobal();
+
+  const transactionList = transactions.map(transaction => transaction.amount);
+  const total = transactionList.reduce((acc, item) => (acc += item), 0).toFixed(2); 
+
   return (
     <>
       <h4>Your Balance</h4>
-      <h1>$0.00</h1>
+      <h1>${total}</h1>
     </>
   );
 };
--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,13 +1,22 @@
+import { useGlobal } from '../context/GlobalState';
+
 const IncomeExpenses = () => {
+  const { transactions } = useGlobal();
+
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const incomeTotal = amounts.filter(amount => amount > 0).reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const expenseTotal = amounts.filter(amount => amount < 0).reduce((acc, item) => (acc += Math.abs(item)), 0).toFixed(2);
+
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p className="money plus">+0.00</p>
+        <p className="money plus">${incomeTotal}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className="money minus">-$0.00</p>
+        <p className="money minus">${expenseTotal}</p>
       </div>
 
     </div>
