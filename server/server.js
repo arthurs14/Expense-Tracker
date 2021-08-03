adb528ddd0ebc49f1cf52a66eaccf83996e4d00a
arthurs14 <arthurcs426@gmail.com>
2021-08-03 16:28:51 -0700

    added db connection to mongodb atlas

--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
 
 dotenv.config({ path: './config/config.env' });
+
+connectDB();
 
 const transactions = require('./routes/transactions');
 
